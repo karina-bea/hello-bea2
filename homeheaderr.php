1f2e8e11b35f9a0026248fb3f8a7ab26741881da
karina-bea <karinabaylocq@Karinas-MacBook-Pro.local>
2013-12-02 10:10:33 -0800

    Homepage Start

---
+++ homeheaderr.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
+
+    <title>Karina Baylocq | UX/UI Designer </title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- <link href='http://fonts.googleapis.com/css?family=Lato:300,400' rel='stylesheet' type='text/css'> -->
+    <!-- <link href='http://fonts.googleapis.com/css?family=Open+Sans|Open+Sans+Condensed:300' rel='stylesheet' type='text/css'> -->
+    <link href="css/hellobea.css" rel="stylesheet">
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+    <script src="javascripts/modernizr-2.6.2.min.js"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <section class="greeter">
+      <div class="container">
+        <div class="col-md-12">
+          <h1>hello, i'm so glad we've finally met.</h1>
+        </div>
+      </div>
+    <!-- Fixed navbar -->
+    <div class="navbar navbar-default" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Karina Bea</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+           
+            <li><a href="#about"><i class="fa fa-camera-retro"></i>About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+             <li class="Projects">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-star-half-o"></i> Featured Projects <i class="fa fa-caret-down"></i></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Art Institute</a></li>
+                <li><a href="bayareanerdgirls.php">Bay Area Nerd Girls</a></li>
+                <li><a href="#">BitTorrent, Inc</a></li>
+                <li class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+    </section>
+
+<div class="container">
---
+++ indexD.php
@@ -0,0 +1,22 @@
+
+<?php include 'homeheaderr.php'; ?>
+<section class="home-grid">
+	<div class="row" >
+		<div class="col-md-3"><a href=""><div class="square1"><h3>Something here</h3></div></a></div>
+		<div class="col-md-3"><a href=""><div class="square2"><h3>Something here</h3></div></a></div>
+		<div class="col-md-3"><a href=""><div class="square3"><h3>Something here</h3></div></a></div>
+		<div class="col-md-3"><a href=""><div class="square4"><h3>Something here</h3></div></a></div>
+	</div>
+	<div class="row">
+		<div class="col-md-6"><a href=""><div class="square5"><h3>Something here</h3></div></a></div>
+		<div class="col-md-3"><a href=""><div class="square6"><h3>Something here</h3></div></a></div>
+		<div class="col-md-3"><a href=""><div class="square7"><h3>Something here</h3></div></a></div>
+	</div>
+	<div class="row">
+		<div class="col-md-3"><a href=""><div class="square8"><h3>Something here</h3></div></a></div>
+		<div class="col-md-3"><a href=""><div class="square9"><h3>Something here</h3></div></a></div>
+		<div class="col-md-6"><a href=""><div class="square10"><h3>Something here</h3></div></a></div>
+	</div>
+</section>
+
+<?php include 'footerr.php'; ?>
