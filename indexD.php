ef545b8abff83a686cad7fbb0896e3fb14e0d620
karina-bea <karinabaylocq@Karinas-MacBook-Pro.local>
2013-12-03 13:43:23 -0800

    Changes to homepage

--- indexD.php
+++ indexD.php
@@ -2,20 +2,20 @@
 <?php include 'homeheaderr.php'; ?>
 <section class="home-grid">
 	<div class="row" >
-		<div class="col-md-3"><a href=""><div class="square1"><h3>Something here</h3></div></a></div>
-		<div class="col-md-3"><a href=""><div class="square2"><h3>Something here</h3></div></a></div>
-		<div class="col-md-3"><a href=""><div class="square3"><h3>Something here</h3></div></a></div>
-		<div class="col-md-3"><a href=""><div class="square4"><h3>Something here</h3></div></a></div>
+		<div class="col-md-3"><a href=""><div class="square1"><h3>BitTorrent, Inc</h3><i class="fa fa-star-half-o"></i></div></a></div>
+		<div class="col-md-3"><a href=""><div class="square2"><h3>Bay Area Nerd Girls</h3><i class="fa fa-female"></i></div></a></div>
+		<div class="col-md-3"><a href=""><div class="square3"><h3>Band League</h3> <i class="fa fa-music"></i></div></a></div>
+		<div class="col-md-3"><a href=""><div class="square4"><h3>Pretty Placeholder</h3><i class="fa fa-flickr"></i></div></a></div>
 	</div>
 	<div class="row">
-		<div class="col-md-6"><a href=""><div class="square5"><h3>Something here</h3></div></a></div>
-		<div class="col-md-3"><a href=""><div class="square6"><h3>Something here</h3></div></a></div>
-		<div class="col-md-3"><a href=""><div class="square7"><h3>Something here</h3></div></a></div>
+		<div class="col-md-6"><a href=""><div class="square5"><h3>Fan vs Fan</h3><i class="fa fa-tumblr-square"></i></div></a></div>
+		<div class="col-md-3"><a href=""><div class="square6"><h3>SF Runner</h3><i class="fa fa-star-half-o"></i></div></a></div>
+		<div class="col-md-3"><a href=""><div class="square7"><h3>AI SF Designs</h3><i class="fa fa-money"></i></div></a></div>
 	</div>
 	<div class="row">
-		<div class="col-md-3"><a href=""><div class="square8"><h3>Something here</h3></div></a></div>
-		<div class="col-md-3"><a href=""><div class="square9"><h3>Something here</h3></div></a></div>
-		<div class="col-md-6"><a href=""><div class="square10"><h3>Something here</h3></div></a></div>
+		<div class="col-md-3"><a href=""><div class="square8"><h3>AI App </h3><i class="fa fa-mobile"></i></div></a></div>
+		<div class="col-md-3"><a href=""><div class="square9"><h3>Open Door Clinic</h3><i class="fa fa-stethoscope"></i></div></a></div>
+		<div class="col-md-6"><a href=""><div class="square10"><h3>Identified</h3><i class="fa fa-search-plus"></i></div></a></div>
 	</div>
 </section>
 
